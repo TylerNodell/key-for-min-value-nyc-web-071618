889b428c71bcbde8ef0da1be84f9fa78f2f1c02f
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 14:59:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowkey = nil
   lowvalue = nil
   name_hash.each do |key, value|
-    if value < lowvalue || lowvalue == nil
+    if lowvalue == nil || value < lowvalue
       lowvalue = value
       lowkey = key
     end
